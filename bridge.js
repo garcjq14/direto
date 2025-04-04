68c279b7f27073fd1b9b8ad06fd5ea2e1903c363
garcjq14 <garcjq14@gmail.com>
2025-04-04 20:46:29 -0300

    Add files via upload

---
+++ bridge.js
@@ -0,0 +1,2 @@
+// Este arquivo serve como ponte entre o HTML e o arquivo principal
+import './src/main.tsx'; 
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    react({
+      include: '**/*.{jsx,tsx,js,ts}',
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  build: {
+    outDir: "dist",
+  }
+}); 
